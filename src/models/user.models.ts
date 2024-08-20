a3f30e1b79fe485439e5e31a0f39e7484bfa22fc
Divy Deshmukh <divydeshmukh1104@gmail.com>
2024-08-20 12:21:07 +0530

    initial commit and added user model

---
+++ src/models/user.models.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { Message } from "postcss";
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
